0acf498aec33e28deef2042451596561f2a7e8c6
Chris-Andiego <74431900+Chris-Andiego@users.noreply.github.com>
2023-05-26 21:41:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Upload from './Components/Upload';
